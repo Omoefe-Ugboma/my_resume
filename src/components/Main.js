986e50938cc5ef1fb60559a1fd0d19cb0eccd658
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-15 00:48:24 +0100

    update 2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+background: ${props => props.theme.body};
+width:100vw;
+height:100vh;
+overflow:hidden;
+
+position:relative;
+
+h2,h3,h4,h5,h6{
+    font-family:'Karla', sans-serif;
+    font-weight:500;
+}
+`
+
+const Container = styled.div`
+padding:2rem;
+`
 
 const Main = () => {
     return (
-        <div>
-            Main Component/Page
-        </div>
+        <MainContainer>
+            <Container>
+                Main Component/Page
+            </Container>
+        </MainContainer>
     )
 }
 
