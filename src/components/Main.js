60d7e3fed7ba715d6961deb98d1f4e4ef0958538
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-15 15:09:55 +0100

    Completed The mailing communication

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import LogoComponent from '../subComponents/LogoComponent'
 import PowerButton from '../subComponents/PowerButton'
@@ -21,6 +22,23 @@ h2,h3,h4,h5,h6{
 const Container = styled.div`
 padding:2rem;
 `
+const Contact = styled(NavLink)`
+color:${props => props.theme.text};
+position:absolute;
+top:2rem;
+right:calc(1rem + 2vw);
+text-decoration:none;
+z-index:1;
+`
+const BLOG = styled(NavLink)`
+color:${props => props.theme.text};
+position:absolute;
+top:50%;
+right:calc(1rem + 2vw);
+transform: rotate(90deg) translate(-50%, -50%);
+text-decoration:none;
+z-index:1;
+`
 
 const Main = () => {
     return (
@@ -29,6 +47,17 @@ const Main = () => {
                 <PowerButton />
                 <LogoComponent />
                 <SocialIcons />
+
+                <Contact target="_blank" to={{ pathname: "mailto:jkomoefey23e@yahoo.co.uk" }}>
+                    <h3>
+                        Say hi...
+                    </h3>
+                </Contact>
+                <BLOG to="/blog">
+                    <h3>
+                        Blog
+                    </h3>
+                </BLOG>
             </Container>
         </MainContainer>
     )
