3f8871e3a98a26661567c2ad41516dff5103a95c
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-15 22:10:27 +0100

    More progress on the home page layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import LogoComponent from '../subComponents/LogoComponent'
 import PowerButton from '../subComponents/PowerButton'
 import SocialIcons from '../subComponents/SocialIcons'
+import { YinYang } from './AllSvgs'
 
 const MainContainer = styled.div`
 background: ${props => props.theme.body};
@@ -39,25 +40,113 @@ transform: rotate(90deg) translate(-50%, -50%);
 text-decoration:none;
 z-index:1;
 `
+const WORK = styled(NavLink)`
+color:${props => props.theme.text};
+position:absolute;
+top:50%;
+left:calc(1rem + 2vw);
+transform: translate(-50%, -50%)  rotate(-90deg) ;
+text-decoration:none;
+z-index:1;
+`
+const BottomBar = styled.div`
+position:absolute;
+bottom:1rem;
+left:0;
+right:0;
+width:100%;
+
+display:flex;
+justify-content:space-evenly;
+`
+const ABOUT = styled(NavLink)`
+color:${props => props.theme.text};
+text-decoration:none;
+z-index:1;
+`
+const SKILLS = styled(NavLink)`
+color:${props => props.theme.text};
+text-decoration:none;
+z-index:1;
+`
+const rotate = keyframes`
+from{
+    transform:rotate(0);
+}
+to{
+    transform:rotate(360deg);
+}
+`
+
+const Center = styled.button`
+position:absolute;
+top:${props => props.click ? '85%' : '50%'};
+left:${props => props.click ? '92%' : '50%'};
+transform:translate(-50%,-50%);
+border:none;
+outline:none;
+background-color:transparent;
+cursor:pointer;
+
+display:flex;
+flex-direction:column;
+justify-conent:center;
+align-items:center;
+transition:all 1s ease;
+
+&>:first-child{
+    animation:${rotate} infinite 1.5s linear;
+}
+
+&>:last-child{
+    display:${props => props.click ? 'none' : 'inline-block'};
+    padding-top: 1rem;
+}
+`
+
 
 const Main = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
     return (
         <MainContainer>
             <Container>
                 <PowerButton />
                 <LogoComponent />
                 <SocialIcons />
+                <DarkDiv click={click} />
+                <Center click={click}>
+                    <YinYang onClick={() => handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
+                    <span>click here</span>
+                </Center>
 
                 <Contact target="_blank" to={{ pathname: "mailto:jkomoefey23e@yahoo.co.uk" }}>
-                    <h3>
+                    <h2>
                         Say hi...
-                    </h3>
+                    </h2>
                 </Contact>
                 <BLOG to="/blog">
-                    <h3>
+                    <h2>
                         Blog
-                    </h3>
+                    </h2>
                 </BLOG>
+                <WORK to="/work">
+                    <h2>
+                        Work
+                    </h2>
+                </WORK>
+                <BottomBar>
+                    <ABOUT to="/about">
+                        <h2>
+                            About
+                        </h2>
+                    </ABOUT>
+                    <SKILLS to="/skills">
+                        <h2>
+                            My Skills
+                        </h2>
+                    </SKILLS>
+                </BottomBar>
             </Container>
         </MainContainer>
     )
--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -15,7 +15,7 @@ z-index:3;
 const LogoComponent = () => {
     return (
         <Logo>
-            Omoefe U. J.
+            O.U
         </Logo>
     )
 }
