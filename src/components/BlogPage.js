238b7b146625336fbd835c904659ff2b8a833721
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-16 10:21:29 +0100

    Blog page in progress

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+import img from '../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg'
+
+
+const MainContainer = styled.div`
+background-image:url(${img});
+background-size:cover;
+background-repeat:no-repeat;
+background-attachment:fixed;
+background-position:center;
+width:100vw;
+height:100vh;
+`
+
+const Container = styled.div`
+background-color:${props => `rgba(${props.theme.bodyRgba},0.8)`};
+width:100%;
+height:auto;
+position:relative;
+padding-bottom:5rem;
+`
 
 export const BlogPage = () => {
     return (
-        <div>
-            BlogPage Component/Page
-        </div>
+        <MainContainer>
+            <Container>
+
+            </Container>
+        </MainContainer>
     )
 }
 
