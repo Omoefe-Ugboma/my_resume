d8c03c2c29cf0714d890eee478b58a599884e5d0
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-17 01:08:26 +0100

    Skill section in progress

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { lightTheme } from './Themes'
 
-export const MySkillsPage = () => {
+const Box = styled.div`
+background-color:${props => props.theme.body};
+width:100vw;
+height:100vh;
+position:relative;
+display:flex;
+justify-content:space-evenly;
+align-items:center;
+`
+const Main = styled.div`
+border: 2px solid ${props => props.theme.text};
+color:${props => props.theme.text};
+background-color:${props => props.theme.body};
+padding: 2rem;
+width:30vw;
+height:60vh;
+z-index:3;
+line-height:1.5;
+`
+
+const MySkillsPage = () => {
     return (
-        <div>
-            MySkillsPage Component/Page
-        </div>
+        <ThemeProvider theme={lightTheme}>
+            <Box>
+                <Main>
+                    MySkillsPage Component/Page
+                </Main>
+            </Box>
+        </ThemeProvider>
     )
 }
 
