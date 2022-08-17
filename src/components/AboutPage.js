eae41df2bc37fd520c7e8eef65ff9c52deaf7f61
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-17 22:34:18 +0100

    About section roboat part in progress

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,11 +1,55 @@
 import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+import { DarkTheme } from './Themes'
 
-export const ABoutPage = () => {
+import LogoComponent from '../subComponents/LogoComponent'
+import SocialIcons from '../subComponents/SocialIcons'
+import PowerButton from '../subComponents/PowerButton'
+import ParticleComponent from '../subComponents/ParticleComponent'
+import astronaut from '../assets/Images/spaceman.png'
+
+const Box = styled.div`
+background-color:${props => props.theme.body};
+width:100vw;
+height:100vh;
+position:relative;
+overflow:hidden; 
+`
+const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }
+`
+
+const Spaceman = styled.div`
+position:absolute;
+top:10%;
+right:5%;
+width:20vw;
+animation: ${float} 4s ease infinite;
+img{
+    width:100%;
+    height:auto;
+}
+
+
+`
+const AboutPage = () => {
     return (
-        <div>
-            ABoutPage Component/Page
-        </div>
+        <ThemeProvider theme={DarkTheme}>
+            <Box>
+
+                <LogoComponent theme='dark' />
+                <SocialIcons theme='dark' />
+                <PowerButton />
+                <ParticleComponent theme='dark' />
+                <Spaceman>
+                    <img src={astronaut} alt="spaceman" />
+                </Spaceman>
+
+            </Box>
+        </ThemeProvider>
     )
 }
 
-export default ABoutPage
+export default AboutPage
