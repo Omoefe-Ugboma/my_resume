7a9e592a351aabdc2cb6561b2c940ee09ef24ca7
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-17 23:25:04 +0100

    Work section in progress

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { DarkTheme } from './Themes'
 
-export const WorkPage = () => {
+import LogoComponent from '../subComponents/LogoComponent'
+import SocialIcons from '../subComponents/SocialIcons'
+import PowerButton from '../subComponents/PowerButton'
+
+import { Work } from "../data/WorkData";
+
+const Box = styled.div`
+background-color:${props => props.theme.body};
+width:100vw;
+height:100vh;
+position:relative;
+overflow:hidden; 
+`
+
+
+const Main = styled.ul`
+ position: fixed;
+ top: 12rem;
+ left: calc(10rem + 15vw);
+ height: 40vh;
+ display: flex;
+
+ color:white;
+`
+
+const WorkPage = () => {
     return (
-        <div>
-            WorkPage Component/Page
-        </div>
+        <ThemeProvider theme={DarkTheme}>
+            <Box>
+
+                <LogoComponent theme='dark' />
+                <SocialIcons theme='dark' />
+                <PowerButton />
+
+                <Main>
+
+                </Main>
+
+            </Box>
+        </ThemeProvider>
     )
 }
 
