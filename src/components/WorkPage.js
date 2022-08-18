1198d1721a99c278cd252267dd2295309a47a5a7
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-18 13:59:48 +0100

    WOrk section completed

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -8,6 +8,7 @@ import PowerButton from '../subComponents/PowerButton'
 
 import { Work } from "../data/WorkData";
 import Card from '../subComponents/Card'
+import { YinYang } from './AllSvgs'
 
 const Box = styled.div`
 background-color:${props => props.theme.body};
@@ -28,13 +29,26 @@ const Main = styled.ul`
  color:white;
 `
 
+const Rotate = styled.span`
+display:block;
+position:fixed;
+right:1rem;
+bottom:1rem;
+width:80px;
+height:80px;
+z-index:1;
+`
+
 const WorkPage = () => {
     const ref = useRef(null);
+    const yinyang = useRef(null);
 
     useEffect(() => {
         let element = ref.current;
         const rotate = () => {
-            element.style.transform = `translateX(${-window.pageXOffset}px)`
+            element.style.transform = `translateX(${-window.pageYOffset}px)`
+
+            yinyang.current.style.transform = `rotate(` + -window.pageYOffset + 'deg)'
         }
         window.addEventListener('scroll', rotate)
 
@@ -56,7 +70,9 @@ const WorkPage = () => {
                         )
                     }
                 </Main>
-
+                <Rotate ref={yinyang}>
+                    <YinYang width={80} fill={DarkTheme.text} />
+                </Rotate>
             </Box>
         </ThemeProvider>
     )
