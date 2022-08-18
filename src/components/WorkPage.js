acbadca6944f6b1d54f0b37755b14f9c772b9e91
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-18 14:34:18 +0100

    BigTitle completed

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -9,13 +9,15 @@ import PowerButton from '../subComponents/PowerButton'
 import { Work } from "../data/WorkData";
 import Card from '../subComponents/Card'
 import { YinYang } from './AllSvgs'
+import BigTitle from '../subComponents/BigTitle'
 
 const Box = styled.div`
 background-color:${props => props.theme.body};
  
 height:400vh;
 position:relative;
-overflow:hidden; 
+display:flex;
+align-items:center;
 `
 
 
@@ -73,6 +75,7 @@ const WorkPage = () => {
                 <Rotate ref={yinyang}>
                     <YinYang width={80} fill={DarkTheme.text} />
                 </Rotate>
+                <BigTitle text="WORK" top='10%' right='20%' />
             </Box>
         </ThemeProvider>
     )
