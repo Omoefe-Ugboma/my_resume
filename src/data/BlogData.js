ac21a5b0a803ba6c12d5095f4b8c179a042662c6
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-19 23:06:43 +0100

    Experience section Update in progress 2

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -2,8 +2,9 @@ export const Blogs = [
     {
         id: 1,
         name: "WEB APPLICATION DEVELOPER",
-        tags: ["react", "gsap", "styled-components"],
-        date: "13 May, 2021",
+        tags: ["react", "styled-components", "Django", "Software Dev", "MySql", "DevOp"],
+        // date: "13 June, 2022",
+        date: "13 June, 2022 - present",
         imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
         link: "https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10"
     },
