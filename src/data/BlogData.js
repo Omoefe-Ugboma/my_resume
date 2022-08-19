7e8d2d6d1aeb4e938b74ac3d7b9c7eded6a839a3
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-19 23:23:43 +0100

    update experience 2

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -2,7 +2,7 @@ export const Blogs = [
     {
         id: 1,
         name: "WEB APPLICATION DEVELOPER",
-        tags: ["react", "styled-components", "Django", "Software Dev", "MySql", "DevOp"],
+        tags: ["Bootstrap", "react", "styled-components", "Django", "Software Dev", "MySql", "DevOp"],
         // date: "13 June, 2022",
         date: "13 June, 2022 - present",
         imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
@@ -10,11 +10,11 @@ export const Blogs = [
     },
     {
         id: 2,
-        name: "How to create React Hamburger Menu using Styled-Components",
-        tags: ["react", "styled-components"],
-        date: "5 July, 2021",
-        imgSrc: "https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link: "https://codebucks.hashnode.dev/react-hamburger-menu"
+        name: "FRONT-END DEVELOPER (ANGULAR)",
+        tags: ["Angular", "Angular Material", "NGRX", "RSJX", "Bootstrap", "Responsive Design"],
+        date: "2021 - 2022",
+        imgSrc: "https://nairametrics.com/wp-content/uploads/2020/06/images.png",
+        link: "https://www.investment-one.com/"
     },
     {
         id: 3,
