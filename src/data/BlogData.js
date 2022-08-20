a0ba4666c81304aa375e2dbc2857dab3d9500584
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-20 17:04:08 +0100

    Update 3

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -22,13 +22,13 @@ export const Blogs = [
         tags: ["HTML5", "CSS3", "Bootstrap", "React", "Redux", "Mentorship"],
         date: "2017 Remotely",
         imgSrc: "https://lh6.googleusercontent.com/-kL8Kko8pDCY/AAAAAAAAAAI/AAAAAAAAAAA/TTtWnda8MM4/s60-c-k-mo/photo.jpg",
-        link: "https://codebucks.hashnode.dev/react-loading-screens"
+        link: "https://promatelink-solutions-limited.business.site/"
     },
     {
         id: 4,
-        name: "How to build a Fabulous Todo App using React, Redux and Framer-Motion",
-        tags: ["react", "redux", "framer-motion"],
-        date: "26 May, 2021",
+        name: "Back-End Web Developer @African Prudential Plc",
+        tags: ["PHP", "codeigniter 3", "Laravel", "MySQL", "PHPUnit Test"],
+        date: "2018",
         imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--r5oUDhhT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/036s7j5b73up7455rdth.png",
         link: "https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1"
     },
