ef38e1cc91b6c028f0743a7f29f1aef0bfc6bb4a
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-20 16:39:37 +0100

    Update on ex[erience

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -18,8 +18,8 @@ export const Blogs = [
     },
     {
         id: 3,
-        name: "React Loading Screen: Try these 3 cool loading screens for your app",
-        tags: ["react", "react-lottie", "styled-components"],
+        name: "Co-Founder and Full-Stack Developer",
+        tags: ["HTML5", "CSS3", "styled-components"],
         date: "6 July, 2021",
         imgSrc: "https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625552344293%2Fegwis0UIX.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
         link: "https://codebucks.hashnode.dev/react-loading-screens"
