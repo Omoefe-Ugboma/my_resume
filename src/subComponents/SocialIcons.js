02a1a14529c107a7ad5ebcdaa2c7aa8b0fc4fba5
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-21 23:41:36 +0100

    Social icon Update

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -44,7 +44,7 @@ const SocialIcons = (props) => {
                 animate={{ scale: [0, 1, 1.5, 1] }}
                 transition={{ type: "spring", duration: 1, delay: 1.2 }}>
                 <NavLink style={{ color: 'inherit' }} target="_blank" to={{ pathname: "https://github.com/Omoefe-Ugboma/AirbnbClone" }}>
-                    <Github width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
+                    <Twitter width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
                 </NavLink>
             </motion.div>
             <motion.div
